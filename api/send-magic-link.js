19a9c21a6c26ee6c0bb7cfc527accc4cc5dcee54
Forrest Miller <fmiller@rzero.com>
2025-08-15 19:58:42 -0700

    Use verified sender email fmiller@rzero.com

--- api/send-magic-link.js
+++ api/send-magic-link.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
           }
         ],
         from: {
-          email: 'rzrobot+customerportal-prod@rzerosystems.com',
+          email: 'fmiller@rzero.com',
           name: 'R-Zero NYC Prospector'
         },
         content: [{
